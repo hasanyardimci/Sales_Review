16dcc7f87410c130ce4402cab31e5a70ecc89327
hsn <hsn@hsns-MacBook-Air.local>
2019-10-19 11:06:25 +0100

    logging class added, two other generator class dropped.

--- converttojson.py
+++ converttojson.py
@@ -11,16 +11,15 @@ def find_file(file_type):
 
 file_array = []
 appPath = os.getcwd() + "/output"
-file_array_all = find_file('txt')
+file_array_all = find_file('dict')
 for file_name in file_array:
     read_file = open(file_name, 'r')
-    write_file = open('C' + file_name.replace('txt', 'json'), 'w')
+    write_file = open('C' + file_name.replace('dict', 'json'), 'w')
     try:
         for line in read_file:
             print(line.replace("'", '"'), file=write_file, end="")
-        os.remove(file_name)
-        os.
-    except (RuntimeError, TypeError, NameError, BrokenPipeError):
+        os.rename(file_name, 'processed/'+file_name)
+    except (RuntimeError, TypeError, NameError, BrokenPipeError, FileExistsError):
         print("Correction Error")
     finally:
         read_file.close()
--- sales.py
+++ sales.py
@@ -1,3 +1,8 @@
+import os
+import time
+import random
+
+
 class Salesman:
 
     def __init__(self, is_manager, e_id, store_id):
@@ -6,7 +11,7 @@ class Salesman:
         self.store_id = store_id
         self.sales_total = 0
 
-    def sales(self, sales_id, sales, item_no, city_name):
+    def sales_dict(self, sales_id, sales, item_no, city_name):
         self.sales_total += sales
         if self.is_manager == 1:
             self.sales_total += round(sales * 1/10)
@@ -17,11 +22,20 @@ class Salesman:
                 "item_no": item_no,
                 "city_name": city_name}
 
-    def write_review(self, review_score, sales_id):
+    def sales_csv(self, sales_id, sales, item_no, city_name):
+        self.sales_total += sales
+        if self.is_manager == 1:
+            self.sales_total += round(sales * 1 / 10)
+        return sales_id, self.e_id, self.store_id, sales, item_no, city_name
+
+    def write_review_dict(self, review_score, sales_id):
         return {"store_id": str(self.store_id),
                 "e_id": str(self.e_id),
                 "review_score": str(review_score),
                 "sales_id": str(sales_id)}
+
+    def write_review_csv(self, review_score, sales_id):
+        return str(self.store_id), str(self.e_id), str(review_score), str(sales_id)
 
 
 class Store:
@@ -39,3 +53,49 @@ class Store:
         manager = Salesman(1, e_id, self.store_id)
         return manager
 
+
+class Logging:
+
+    def __init__(self, app_dir, file_extension, random_review):
+        self.random_review = random_review
+        self.app_dir = app_dir
+        self.file_extension = file_extension
+        self.file_name_sales = app_dir + 'sales_' + str(round(time.time())) + '.' + file_extension
+        self.file_name_review = app_dir + 'review_' + str(round(time.time())) + '.' + file_extension
+        self.store_1 = Store(random.randint(1, 10000),
+                             random.choice(['Richmond', 'Ealing', 'Barnet', 'Hounslow', 'Merton', 'Westmister']))
+        self.store_2 = Store(random.randint(1, 10000),
+                             random.choice(['Richmond', 'Ealing', 'Barnet', 'Hounslow', 'Merton', 'Westmister']))
+        self.manager_1 = self.store_1.hired_manager(random.randint(1, 10000))
+        self.manager_2 = self.store_2.hired_manager(random.randint(1, 10000))
+        self.salesman_11 = self.store_1.hired_salesman(random.randint(1, 10000))
+        self.salesman_12 = self.store_1.hired_salesman(random.randint(1, 10000))
+        self.salesman_21 = self.store_2.hired_salesman(random.randint(1, 10000))
+        self.salesman_22 = self.store_2.hired_salesman(random.randint(1, 10000))
+
+    def logging_events(self):
+        for _ in range(1):
+            f_sales = open(self.file_name_sales, 'w')
+            f_review = open(self.file_name_review, 'w')
+            for _ in range(10000):
+                random_salesman = random.choice([self.salesman_11, self.salesman_12,
+                                                 self.salesman_21, self.salesman_22,
+                                                 self.manager_2, self.manager_1])
+                invoice_id = random.randint(1, 10000000)
+                print(random_salesman.sales_csv(invoice_id,
+                                                random.randint(1, 10000),
+                                                random.randint(1, 10000),
+                                                random.choice(
+                                                    ['Richmond', 'Ealing', 'Barnet', 'Hounslow', 'Merton', 'Westmister']
+                                                             )), file=f_sales)
+                if self.random_review == random.choice([1, 2, 3, 4, 5, 6]):
+                    print(random_salesman.write_review_csv(
+                        random.randint(1, 10),
+                        invoice_id), file=f_review)
+            f_sales.close()
+            f_review.close()
+
+
+if __name__ == "__main__":
+    logging_start = Logging('output/', 'cvs', 1)
+    logging_start.logging_events()
